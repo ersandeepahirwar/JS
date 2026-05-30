bfa5f627d9d88688d6599242ed0386ce8151ad54
Sandeep Ahirwar <codeysandeep@gmail.com>
2026-05-30 15:23:06 +0530

    Updated script.js

--- Exercises/@Ducat/Exercise - 034/script.js
+++ Exercises/@Ducat/Exercise - 034/script.js
@@ -1,4 +1,4 @@
-// Write a JavaScript program to print and count all Armstrong Numbers within a given range.
+// Write a JavaScript program to print and count all 3-Digits Armstrong Numbers within a given range.
 
 let startingRange = parseInt(prompt("What is your staring range?"));
 let endingRange = parseInt(prompt("What is your ending range?"));
@@ -17,7 +17,7 @@ for (let number = startingRange; number <= endingRange; number++) {
     sumOfDigitsCubes += Math.pow(digit, 3);
   }
 
-  if (sumOfDigitsCubes === number) {
+  if (number > 1 && sumOfDigitsCubes === number) {
     armstrongNumbersCount++;
     armstrongNumbers += ` ${number}`;
   }
@@ -25,7 +25,7 @@ for (let number = startingRange; number <= endingRange; number++) {
 
 if (armstrongNumbersCount > 0) {
   alert(
-    `${armstrongNumbersCount} Armstrong Number${armstrongNumbersCount > 1 ? "s" : ""} found between ${startingRange} and ${endingRange} and ${armstrongNumbersCount > 1 ? "They are" : "It is"}${armstrongNumbers}`,
+    `${armstrongNumbersCount} 3-Digits Armstrong Number${armstrongNumbersCount > 1 ? "s" : ""} found between ${startingRange} and ${endingRange} and ${armstrongNumbersCount > 1 ? "They are" : "It is"}${armstrongNumbers}`,
   );
 } else {
   alert(
